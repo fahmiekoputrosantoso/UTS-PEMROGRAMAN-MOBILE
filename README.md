b1d140581561998c07ece790b2ff8ed95479e544
fahmiekoputrosantoso <fahmieko73@gmail.com>
2023-05-19 16:49:29 +0700

    flutter

---
+++ README.md
@@ -0,0 +1,28 @@
+# PEMROGRAMAN MOBILE
+## UJIAN TENGAH SEMESTER
+
+
+| Nama       | NIM        | Jurusan      |
+|------------|------------|--------------|
+| Fahmi Eko Putro Santoso   | 312010046 | Teknik Informatika |
+
+## Login view
+
+![assets](uts_from_login/assets/hasil1.png)
+
+## Register
+
+![assets](uts_from_login/assets/hasil2.png)
+
+## Reset password
+
+![assets](uts_from_login/assets/hasil3.png)
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ uts_from_login/lib/register.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+
+class myRegister extends StatefulWidget {
+  const myRegister({Key? key}) : super(key: key);
+
+  @override
+  _myRegisterState createState() => _myRegisterState();
+}
+
+class _myRegisterState extends State<myRegister> {
+  //Password Field obscureText  Handler
+  bool _isHidden = true;
+  void _toggleVisibility() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/register.png',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          appBar: AppBar(
+              elevation: null,
+              backgroundColor: Colors.white,
+              leading: TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'login');
+                },
+                child: Icon(
+                  Icons.arrow_back_ios_rounded,
+                  color: Colors.white,
+                ),
+              )),
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'REGISTER',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 111, 141, 240),
+                      fontSize: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.28,
+                    left: 35,
+                    right: 35,
+                  ),
+                  child: Column(
+                    children: [
+                      TextField(
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        decoration: InputDecoration(
+                          labelText: 'New Username',
+                          fillColor: Color.fromARGB(255, 94, 94, 94),
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: const BorderSide(
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              )),
+                              labelStyle: TextStyle(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      TextField(
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                        ),
+                        decoration: InputDecoration(
+                          fillColor: Color.fromARGB(255, 95, 95, 95),
+                          prefixIcon: Icon(Icons.email_outlined),
+                          filled: true,
+                          labelText: 'Masukan Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          labelStyle: TextStyle(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      TextField(
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        decoration: InputDecoration(
+                          fillColor: Color.fromARGB(255, 95, 95, 95),
+                          prefixIcon: Icon(Icons.phone),
+                          filled: true,
+                          labelText: 'Nomor handphone',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          labelStyle: TextStyle(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      TextField(
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        obscureText: _isHidden,
+                        decoration: InputDecoration(
+                          fillColor: Color.fromARGB(255, 95, 95, 95),
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            onPressed: _toggleVisibility,
+                            icon: _isHidden
+                                ? Icon(Icons.visibility)
+                                : Icon(Icons.visibility_off),
+                          ),
+                          labelText: 'Masukan Password',
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                            ),
+                          ),
+                          labelStyle: TextStyle(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                maximumSize: const Size(170.0, 90.0),
+                                minimumSize: const Size(170.0, 60.0),
+                                primary: Color.fromARGB(255, 95, 95, 95),
+                                shape: const StadiumBorder(),
+                              ),
+                              onPressed: () {},
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                //crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text('REGISTER'),
+                                  Icon(
+                                    Icons.content_paste_rounded,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ),
+                      SizedBox(height: 30.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'login');
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 0, 0, 0)),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'forgot');
+                            },
+                            child: Text(
+                              'Forgot password?',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
